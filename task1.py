aa5c48fc063f326190634703dbcf05ef4f7d40d5
Janvi Tikyani <barkhatikyani@gmail.com>
2025-09-05 08:45:43 +0000

    Calculator

---
+++ task1.py
@@ -0,0 +1,11 @@
+#simple calculator
+a=input('Enter first number:')
+a=int(a)
+b=input('Enter second number:')
+b=int(b)
+print('Addition:',a+b)
+print('Subtraction:',a-b)
+print('Multiplication',a*b)
+print('Division:',float(a/b))
+
+
---
+++ task2.py
@@ -0,0 +1,3 @@
+fname=input('Enter your First Name:')
+lname=input('Enter your Last Number')
+print("Hello, " + fname  + lname + " Welcome! to the Python Program")
